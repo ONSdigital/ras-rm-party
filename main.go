0157080f3d5edb402b1b6c13f30467259e1dab73
Matt Crocker <matt.crocker@gmail.com>
2020-02-10 12:20:35 +0000

    Basic Unleash feature flagging test

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/Unleash/unleash-client-go/v3"
 	"github.com/julienschmidt/httprouter"
 )
 
 func hello(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "ras-rm-party")
+	if unleash.IsEnabled("party.api.get.hello") {
+		fmt.Fprint(w, "ras-rm-party")
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
 }
 
 func addRoutes(r *httprouter.Router) {
@@ -16,6 +22,10 @@ func addRoutes(r *httprouter.Router) {
 }
 
 func main() {
+	unleash.Initialize(unleash.WithListener(&unleash.DebugListener{}),
+		unleash.WithAppName("ras-rm-party"), unleash.WithUrl("http://localhost:4242/api"))
 	router := httprouter.New()
 	addRoutes(router)
+
+	log.Fatal(http.ListenAndServe(":6969", router))
 }
