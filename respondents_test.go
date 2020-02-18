890c6a319ec227460667eba94857c1e7f8d3d136
Matt Crocker <matt.crocker@gmail.com>
2020-02-18 11:53:33 +0000

    Test refactor

--- respondents_test.go
+++ respondents_test.go
@@ -8,6 +8,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestGetRespondentsIsFeatureFlagged(t *testing.T) {
+	// Assure that it's properly feature flagged away
+	setDefaults()
+	setup()
+
+	req := httptest.NewRequest("GET", "/v2/respondents/", nil)
+	router.ServeHTTP(resp, req)
+
+	assert.Equal(t, http.StatusMethodNotAllowed, resp.Code)
+}
 func TestGetRespondents(t *testing.T) {
 	setDefaults()
 	setup()
