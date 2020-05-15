eb7df99f70768c3644c0b339153fe72f51be7094
Matt Crocker <matt.crocker@gmail.com>
2020-05-15 13:32:40 +0100

    Trigger build

--- info.go
+++ info.go
@@ -11,7 +11,7 @@ import (
 
 func getInfo(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	info := models.Info{
-		Name:    "Hello world",
+		Name:    "Hello world 2",
 		Version: viper.GetString("app_version"),
 	}
 	w.Header().Set("Content-Type", "application/json")
